224a4eb1cf30f4a775e1cce2a4ce232383e26a2c
KinhCode <61858392+KinhCode@users.noreply.github.com>
2026-03-29 01:02:00 +0700

    Added Import ChatPanel from @mariozechner/pi-web-ui and updated the RiverCityChatBot component to use it. Also updated the supported languages list and made some UI adjustments.

--- src/components/RiverCityChatBot.jsx
+++ src/components/RiverCityChatBot.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { PiChat } from '@mariozechner/pi-web-ui';
+import { ChatPanel } from '@mariozechner/pi-web-ui';
 import './RiverCityChatBot.css';
 
 const SUPPORTED_LANGUAGES = [
